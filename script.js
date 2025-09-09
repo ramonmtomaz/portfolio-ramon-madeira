9683116193f3347943e83bf6d0c4ff4430231fe4
Ramon Madeira Tomaz <ramonmadeira5@gmail.com>
2025-09-09 15:11:57 -0300

    Refactor event listeners for display toggling

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-document.getElementById("seta-esquerda").addEventListener("click", function() {
-  if (document.querySelector(".audestoque").style.display === "none") {
+document.getElementById("seta-esquerda").addEventListener("click", function () {
+  if (document.querySelector(".cftv").style.display === "flex") {
+    document.querySelector(".audpedido").style.display = "flex";
+    document.querySelector(".audestoque").style.display = "none";
+    document.querySelector(".cftv").style.display = "none";
+  } else if (document.querySelector(".audpedido").style.display === "flex") {
     document.querySelector(".audestoque").style.display = "flex";
     document.querySelector(".audpedido").style.display = "none";
-  } else {
+    document.querySelector(".cftv").style.display = "none";
+  } else if (document.querySelector(".audestoque").style.display === "flex") {
+    document.querySelector(".cftv").style.display = "flex";
     document.querySelector(".audestoque").style.display = "none";
-    document.querySelector(".audpedido").style.display = "flex";
+    document.querySelector(".audpedido").style.display = "none";
   }
 });
 
-document.getElementById("seta-direita").addEventListener("click", function() {
-  if (document.querySelector(".audpedido").style.display === "none") {
-    document.querySelector(".audpedido").style.display = "flex";
-    document.querySelector(".audestoque").style.display = "none";
-  } else {
+document.getElementById("seta-direita").addEventListener("click", function () {
+  if (document.querySelector(".cftv").style.display === "flex") {
     document.querySelector(".audpedido").style.display = "none";
     document.querySelector(".audestoque").style.display = "flex";
+    document.querySelector(".cftv").style.display = "none";
+  } else if (document.querySelector(".audpedido").style.display === "flex") {
+    document.querySelector(".audestoque").style.display = "none";
+    document.querySelector(".audpedido").style.display = "none";
+    document.querySelector(".cftv").style.display = "flex";
+  } else if ((document.querySelector(".audestoque").style.display = "flex")) {
+    document.querySelector(".cftv").style.display = "none";
+    document.querySelector(".audestoque").style.display = "none";
+    document.querySelector(".audpedido").style.display = "flex";
   }
 });
